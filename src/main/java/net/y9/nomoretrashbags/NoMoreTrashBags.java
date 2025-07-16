908777515743008a98e168acba9bc00984aa8f30
y9dev <151707122+y9dev@users.noreply.github.com>
2025-07-16 19:10:29 +0500

    Prepared environment

---
+++ src/main/java/net/y9/nomoretrashbags/NoMoreTrashBags.java
@@ -0,0 +1,14 @@
+package net.y9.nomoretrashbags;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoMoreTrashBags implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
